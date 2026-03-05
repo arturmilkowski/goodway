fd71724c70cf7869e671017fcf879e83acf5b4ad
Artur Miłkowski <artur-milkowski@tlen.pl>
2026-03-05 12:45:07 +0100

    Dodanie seedera User

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'artur',
+            'email' => 'artur-milkowski@tlen.pl',
+            'email_verified_at' => now(),
+            'password' => bcrypt('12345678'),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'krzysztof',
+            'email' => 'krzysztof@tlen.pl',
+            'email_verified_at' => now(),
+            'password' => bcrypt('12345678'),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+        ]);
+    }
+}
