ac7d951c7c9c2fd6be79b1fba357ee74d58ca7f1
Artur Miłkowski <artur-milkowski@tlen.pl>
2026-05-03 17:21:30 +0200

    Poprawa adresu login (po dodaniu tłumaczeń)

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -17,7 +17,7 @@ defineProps<{
             <div class="flex flex-col gap-8">
                 <div class="flex flex-col items-center gap-4">
                     <Link
-                        :href="home()"
+                        href="/pl"
                         class="flex flex-col items-center gap-2 font-medium"
                     >
                         <div
